346337586408774774a9c0af5501f6d83a8b51c4
Eduardo <eduardovjoliveira@gmail.com>
2026-03-07 18:10:14 -0300

    add aula variavel

---
+++ variavel.cs
@@ -0,0 +1,27 @@
+//no float sempre tem que usar o 'f' no final
+//constate = 'const' ela nao muda
+int minhaIdade = 21;
+string meunome = "Eduardo";
+float velocidadeF1 = 256.5f;
+bool campeao = true;
+const float PI = 3.145667f;
+
+Console.WriteLine(minhaIdade);
+Console.WriteLine(meunome);
+Console.WriteLine(velocidadeF1);
+Console.WriteLine(campeao);
+Console.WriteLine(PI);
+
+//Alterando o valor da variável
+
+velocidadeF1 = 286.4f;
+campeao = false;
+
+Console.WriteLine(velocidadeF1);
+Console.WriteLine(campeao);
+
+
+
+
+
+Console.ReadLine();
