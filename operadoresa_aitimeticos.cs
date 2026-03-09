a85f37fb47a34d1378ada4bd250e7e39d341c938
Eduardo <eduardovjoliveira@gmail.com>
2026-03-09 12:16:56 -0300

    add operadoresa_aitimeticos.cs

---
+++ operadoresa_aitimeticos.cs
@@ -0,0 +1,10 @@
+int numero = 54 + 33;
+int numeroMult =30*5;
+float numeroDiv = 5.0f/2;
+
+Console.WriteLine(numero);
+Console.WriteLine(numeroMult);
+Console.WriteLine(numeroDiv);
+
+
+Console.ReadLine();
