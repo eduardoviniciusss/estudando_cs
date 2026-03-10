d61d3f54545b00f4fb747e46962b88f51b735537
Eduardo <eduardovjoliveira@gmail.com>
2026-03-10 19:19:39 -0300

    operadoreslogicos.cs

--- operadoreslogicos.cs
+++ operadoreslogicos.cs
@@ -1,7 +1,15 @@
+
+//&&(E ou and) e || (OU/OR)
+//SER MULHER E TER >=25 (TRUE)\
+//MULHER(V) && 45 ANOS(V) (TRUE)
+
+//MULHER(V) && 18 anos(f) (false)
+
+
 Console.WriteLine("Digite sua idade:");
 int idade = int.Parse(Console.ReadLine());
 
-if (idade >= 0 && idade <=11)
+if (idade >= 0 && idade <=11)//&& "e"
 {
     Console.WriteLine("Voçê é uma criança!");
 }
