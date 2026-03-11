17a73326e0b714828003ed2b65ac26f41f399623
Eduardo <eduardovjoliveira@gmail.com>
2026-03-11 13:15:27 -0300

    add  funcao.cs

---
+++ funcao.cs
@@ -0,0 +1,18 @@
+
+GerarPreco(28);
+GerarPreco(11);
+GerarPreco(48);
+GerarPreco(60);
+
+static void ExibidorDeMsg()
+{
+   Console.WriteLine("Testando função");
+   Console.WriteLine("Eduardo Vinicius José");
+   
+}
+
+static void GerarPreco(int preco)
+{
+    int valorFinal = preco + (2 * preco);
+    Console.WriteLine("Valor Final:"+valorFinal);
+}
