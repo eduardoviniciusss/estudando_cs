9c2f6831306e146c9e138ead2766ed283fce6698
Eduardo <eduardovjoliveira@gmail.com>
2026-03-11 12:37:15 -0300

    add testeIfElse.cs

---
+++ testeIfElse.cs
@@ -0,0 +1,18 @@
+
+
+using System.Diagnostics;
+
+Console.WriteLine("Digite sua idade:");
+int idade = int.Parse(Console.ReadLine());
+Console.WriteLine("Você tem convite (true/false)?");
+bool temConvite = bool.Parse(Console.ReadLine());
+
+
+if(idade >=18 && true)
+{
+    Console.WriteLine("pode entar!");
+}
+else
+{
+    Console.WriteLine("não pode entar!");
+}
