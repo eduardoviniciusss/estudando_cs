b44ba989666074ffa0c47a613801111ae04cf2a4
Eduardo <eduardovjoliveira@gmail.com>
2026-03-14 11:42:28 -0300

    add funcao.cs

--- funcao.cs
+++ funcao.cs
@@ -1,8 +1,18 @@
-
+ /*
 GerarPreco(28);
 GerarPreco(11);
 GerarPreco(48);
 GerarPreco(60);
+ */
+ 
+ int soma1 = Somar(1,2,3);
+ int soma2 = Somar(10,20,30);
+ int soma3 = Somar(100,200,300);
+
+ Console.WriteLine(soma1);
+ Console.WriteLine(soma2);
+ Console.WriteLine(soma3);
+
 
 Console.ReadLine();
 
@@ -18,3 +28,12 @@ static void GerarPreco(int preco)
     int valorFinal = preco + (2 * preco);
     Console.WriteLine("Valor Final:"+valorFinal);
 }
+
+//Retorno de Função
+
+static int Somar(int a ,int b, int c)
+{
+    int resuntadoFinal = a + b + c;
+    return resuntadoFinal;
+}
+
