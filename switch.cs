228c8bb54eac659ba0c7673126a86bb6ea076f1d
Eduardo <eduardovjoliveira@gmail.com>
2026-03-14 16:45:03 -0300

    add switch.cs

---
+++ switch.cs
@@ -0,0 +1,25 @@
+string cor = "Roxo";
+
+switch (cor)
+{
+    case "Azul":
+    Console.WriteLine("Sua cor favorita é Azul!");
+    break;
+    case "Rosa":
+    Console.WriteLine("Sua cor favorita é Rosa");
+    break;
+    case "Verde":
+    Console.WriteLine("Sua cor favorita é Verde!");
+    break;
+    case "Amarelo":
+    Console.WriteLine("Sua cor favorita é Amarelo!");
+    break;
+    default:
+    Console.WriteLine("Sua cor favorita não tem!");
+    break;
+}
+
+
+
+
+Console.ReadLine();
