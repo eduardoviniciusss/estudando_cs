097a8ee15c8d0f8be8b572310ec09b677cffbf59
Eduardo <eduardovjoliveira@gmail.com>
2026-03-15 16:34:36 -0300

    enum.cs

---
+++ enum.cs
@@ -0,0 +1,15 @@
+//Enum ser pra limitar minhas variaves, só usar oq estar dnetro enum
+
+
+
+
+Cor favorita = Cor.Rosa;
+Cor favoritaDaAna = Cor.Roxo;
+
+Console.WriteLine((int)favorita);
+Console.WriteLine(favoritaDaAna);
+
+Console.ReadLine();
+
+enum Cor {Azul, Rosa, Amarelo, Roxo}
+
