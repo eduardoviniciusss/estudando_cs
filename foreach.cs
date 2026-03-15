f28432acb2f1c3567e58180729b1a60d896fe77c
Eduardo <eduardovjoliveira@gmail.com>
2026-03-15 19:03:26 -0300

    add foreach

---
+++ foreach.cs
@@ -0,0 +1,15 @@
+string[] palavras = new string[5]
+{
+    "Roda",// 0
+ "Pneus",//1
+ "Cama de ar",//2
+ "Carlota",//3
+ "Borracha"//4 
+};
+
+foreach(string palavra in palavras)
+{
+    Console.WriteLine(palavra);
+}
+
+Console.ReadLine();
