aa6d9b8199a05ce4b771b4f578dc1bae3a25d201
Eduardo <eduardovjoliveira@gmail.com>
2026-03-15 18:47:38 -0300

    add doWhile

---
+++ doWhile.cs
@@ -0,0 +1,11 @@
+int numero = 10;
+do
+{
+ Console.WriteLine("do while");
+ numero +=1;
+ Console.WriteLine("numero");
+ 
+}while(1000>numero);
+Console.WriteLine("FIM DA LINHA!");
+
+Console.ReadLine();
