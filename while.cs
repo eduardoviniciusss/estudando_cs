8399801f3417731bd5b1f406baa99538a0aaadfa
Eduardo <eduardovjoliveira@gmail.com>
2026-03-15 18:33:15 -0300

    add while

---
+++ while.cs
@@ -0,0 +1,14 @@
+
+int contador = 0;
+
+while(contador<10)// "While" em quanto for verdade faça/loop infinito!
+{
+    Console.WriteLine(contador);
+    Console.WriteLine("Rodando While");
+  // cada vez que executar vai somar 1 quando chega da decima ele vai dar falsa pq vai ser maior que 10.
+  contador = contador +1;
+}
+Console.WriteLine("FIM DA LINHA");
+Console.ReadLine();
+
+
