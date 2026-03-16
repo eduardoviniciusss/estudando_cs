2b6460bc8269679a47dd0b6546cce33d3a2a6a07
Eduardo <eduardovjoliveira@gmail.com>
2026-03-16 11:32:27 -0300

    add for.cs

---
+++ for.cs
@@ -0,0 +1,22 @@
+string[] cor = new string[5]
+{
+    "Amarelo",
+    "Roxo",
+    "Azul",
+    "Verde",
+    "Vermelho"
+};
+
+for(int contador = 0;contador<10;contador += 1)
+{
+    Console.WriteLine(contador);
+    Console.WriteLine(cor[contador]);
+}
+
+
+
+
+
+
+
+Console.ReadLine();
